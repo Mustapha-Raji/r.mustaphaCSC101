21889e1b54828d026edeb2edcb95f5baa4194c2a
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-10-15 16:54:57 +0100

    My practice_5 commit

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let special_character = "@"; //default
+    let alphabet:char = 'B';
+    let surname = "Raji";
+    let first_name = "Mustapha";
+    let middle_name = "Olz";
+
+    println!(" ");
+    println!("Special_character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+
+
+}
+
