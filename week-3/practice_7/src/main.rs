f8ecdb999f4d2f7a2fc68ad643ff6f12a4f562b3
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-10-15 20:07:10 +0100

    My practice_7 commit

---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let float_with_operator = 20_000_0000;
+    println!("float value {}", float_with_operator);
+
+    let int_with_operator = 15_000;
+    println!("int value {}", int_with_operator);
+}
