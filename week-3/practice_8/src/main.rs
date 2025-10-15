413aeef3cd5535d88d4b6d7bbc82e11f11186d3c
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-10-15 20:16:08 +0100

    My practice_8 commit

---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let fees = 20_000;
+    println!("fees is {}", fees);
+
+    let tuition = 23_500;
+    println!("fees changed is {}",tuition);
+}
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+/Users/mustapharaji/r.mustaphaCSC101/week-3/practice_8/target/debug/practice_8: /Users/mustapharaji/r.mustaphaCSC101/week-3/practice_8/src/main.rs
