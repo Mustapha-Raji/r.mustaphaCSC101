fb649c8273217a2bf1e00f604240e5ee9faf606b
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-05 16:15:50 +0100

    My week 5 practice 1 commit

---
+++ week_6/Practice_1/target/debug/deps/praice_1-59ddf113010a2216.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/Practice_1/target/debug/deps/praice_1-59ddf113010a2216.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week_6/Practice_1/target/debug/deps/praice_1-59ddf113010a2216: src/main.rs
+
+src/main.rs:
---
+++ week_6/Practice_1/target/debug/praice_1.d
@@ -0,0 +1,1 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/Practice_1/target/debug/praice_1: /Users/mustapharaji/r.mustaphaCSC101/week_6/Practice_1/src/main.rs
