eabf1506469cb4c9c5f6fbc9e64ade71e71c51b9
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-05 16:31:27 +0100

    My week6 practice 3 commit

---
+++ week_6/practice_3/target/debug/deps/week_3-50984478e63d9fbe.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_3/target/debug/deps/week_3-50984478e63d9fbe.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_3/target/debug/deps/week_3-50984478e63d9fbe: src/main.rs
+
+src/main.rs:
