c6c925ad4796371aa6452d80054d3e72371e77f1
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-05 19:26:47 +0100

    My week 6 practice 11 commit

---
+++ week_6/practice_11/target/debug/deps/practice_11-c802a64f138902dd.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_11/target/debug/deps/practice_11-c802a64f138902dd.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_11/target/debug/deps/practice_11-c802a64f138902dd: src/main.rs
+
+src/main.rs:
---
+++ week_6/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_11/target/debug/practice_11: /Users/mustapharaji/r.mustaphaCSC101/week_6/practice_11/src/main.rs
