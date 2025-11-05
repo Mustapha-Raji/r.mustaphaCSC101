72d78c0e1d94f029df6d1526e19cedc252d7be99
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-05 16:24:48 +0100

    My week 2 practice 2 commit

---
+++ week_6/practice_2/target/debug/deps/practice_2-f04de46f28236889.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_2/target/debug/deps/practice_2-f04de46f28236889.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week_6/practice_2/target/debug/deps/practice_2-f04de46f28236889: src/main.rs
+
+src/main.rs:
