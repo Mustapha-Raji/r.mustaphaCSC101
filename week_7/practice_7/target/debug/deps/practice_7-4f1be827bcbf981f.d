06f313e06cd87c45fce55bd4ef8fc60297cff00c
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-12 16:57:57 +0100

    My week 7 practice 7 commit

---
+++ week_7/practice_7/target/debug/deps/practice_7-4f1be827bcbf981f.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week_7/practice_7/target/debug/deps/practice_7-4f1be827bcbf981f.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week_7/practice_7/target/debug/deps/practice_7-4f1be827bcbf981f: src/main.rs
+
+src/main.rs:
