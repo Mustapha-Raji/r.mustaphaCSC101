3f7635999cc5662bb5791986408b7ad4634a799c
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-16 23:08:06 +0100

    Week 7 project commit

---
+++ week-4/project_2/practice_x/target/debug/deps/practice_x-b929c1e0d744344a.d
@@ -0,0 +1,5 @@
+/Users/mustapharaji/r.mustaphaCSC101/week-4/project_2/practice_x/target/debug/deps/practice_x-b929c1e0d744344a.d: src/main.rs
+
+/Users/mustapharaji/r.mustaphaCSC101/week-4/project_2/practice_x/target/debug/deps/practice_x-b929c1e0d744344a: src/main.rs
+
+src/main.rs:
---
+++ week-4/project_2/practice_x/target/debug/practice_x.d
@@ -0,0 +1,1 @@
+/Users/mustapharaji/r.mustaphaCSC101/week-4/project_2/practice_x/target/debug/practice_x: /Users/mustapharaji/r.mustaphaCSC101/week-4/project_2/practice_x/src/main.rs
