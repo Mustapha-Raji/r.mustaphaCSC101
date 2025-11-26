038859ecd9efffa6484bbd0eafa8a2cc1c99b2a3
Mustapha Raji <mustapha.raji@pau.edu.ng>
2025-11-26 19:34:02 +0100

    Code for appending data in a rust file

---
+++ week9/practice4/src/main.rs
@@ -0,0 +1,25 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+
+
+fn main() {
+    let mut file = std::fs::File::create("Data.txt").expect("Create failed");
+    file.write_all("Data text file for rust programming\n"
+        .as_bytes()).expect("write failed");
+
+
+
+    let mut file = OpenOptions::new().append(true).open("Data.txt").expect("Cannot open file");
+    file.write_all("\nHello Class".as_bytes()).expect("write failed");
+    file.write_all("\nThis is the appendage to the document."
+        .as_bytes()).expect("Write failed");
+    println!("file append success");
+
+
+
+
+
+
+
+}
